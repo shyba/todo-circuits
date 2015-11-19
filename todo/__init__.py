fd84706bacf1408806214428e02de09f994a755f
Victor Shyba <victor.shyba@gmail.com>
2015-11-18 23:17:43 -0300

    add OPTIONS support

--- todo/__init__.py
+++ todo/__init__.py
@@ -5,13 +5,20 @@ from circuits.web import Server, JSONController
 
 
 class Root(JSONController):
+    METHODS = 'POST, HEAD, GET, OPTIONS, DELETE'
 
     def _set_CORS(self):
         self.response.headers["Access-Control-Allow-Origin"] = "*"
+        self.response.headers['Access-Control-Allow-Methods'] = self.METHODS
+        self.response.headers["Access-Control-Allow-Headers"] = "Content-Type"
 
     def index(self):
         self._set_CORS()
-        return {"success": True}
+        return []
+
+    def OPTIONS(self):
+        self.response.headers['Allow'] = self.METHODS
+        self._set_CORS()
 
 
 def run():
