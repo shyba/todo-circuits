d4e3cb356292ff8a2b732afe80f2dbeb80d4efaa
Victor Shyba <victor.shyba@gmail.com>
2015-11-18 21:21:15 -0300

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+requires = [
+    'circuits',
+    ]
+
+setup(name='TODO Circuits',
+      version='0.0',
+      description='TODO Backend with Circuits frameworks',
+      long_description=README,
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Victor Shyba',
+      author_email='victor.shyba@gmail.com',
+      url='https://github.com/shyba/todo-circuits',
+      keywords='todo backend circuits example',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="tests",
+      entry_points="""\
+      [paste.app_factory]
+      main = todo:main
+      """,
+      )
