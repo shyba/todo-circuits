de7c3c8c546576bf32044343fd7b3fb27e849b49
Victor Shyba <victor.shyba@gmail.com>
2015-11-19 20:18:19 -0300

    Extract header values from resource

--- todo/__init__.py
+++ todo/__init__.py
@@ -3,22 +3,30 @@
 import os
 from circuits.web import Server, JSONController
 
+METHODS = 'POST, HEAD, GET, OPTIONS, DELETE'
+
+
+CORS_HEADERS = {}
+CORS_HEADERS["Access-Control-Allow-Origin"] = "*"
+CORS_HEADERS['Access-Control-Allow-Methods'] = METHODS
+CORS_HEADERS["Access-Control-Allow-Headers"] = "Content-Type"
+
 
 class Root(JSONController):
-    METHODS = 'POST, HEAD, GET, OPTIONS, DELETE'
 
     def _set_CORS(self):
-        self.response.headers["Access-Control-Allow-Origin"] = "*"
-        self.response.headers['Access-Control-Allow-Methods'] = self.METHODS
-        self.response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        self.response.headers.update(CORS_HEADERS)
 
     def index(self):
         self._set_CORS()
         return []
 
     def OPTIONS(self):
-        self.response.headers['Allow'] = self.METHODS
+        self.response.headers['Allow'] = METHODS
         self._set_CORS()
+
+    def POST(self, **args):
+        return args
 
 
 def run():
